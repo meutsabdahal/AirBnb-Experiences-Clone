da1308abd37d6445667ba1f9134a65dcf49441d7
Utsab Dahal <70103872+meutsabdahal@users.noreply.github.com>
2022-10-28 12:11:28 +0545

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import logo from "../img/airbnb-logo.png"
+
+function NavBar() {
+    return (
+        <>
+        <nav>
+            <img src = {logo} className = 'logo'/>
+
+
+        </nav>
+
+        </>
+    )
+}
+
+export default NavBar
